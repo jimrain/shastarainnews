3da4094fa20d4d698df6cdbba6f90d6d1462cb09
jimrain <jim@rainville.net>
2020-06-16 16:06:03 -0700

    Adding 'id' field to all the serializers.

--- pmvc/serializers.py
+++ pmvc/serializers.py
@@ -7,21 +7,21 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
     url = serializers.HyperlinkedIdentityField(view_name="pmvc:user-detail")
     class Meta:
         model = User
-        fields = ['url', 'username', 'email', 'groups']
+        fields = ['url', 'id', 'username', 'email', 'groups']
 
 
 class GroupSerializer(serializers.HyperlinkedModelSerializer):
     url = serializers.HyperlinkedIdentityField(view_name="pmvc:group-detail")
     class Meta:
         model = Group
-        fields = ['url', 'name']
+        fields = ['url', 'id', 'name']
 
 
 class AccountSerializer(serializers.HyperlinkedModelSerializer):
     url = serializers.HyperlinkedIdentityField(view_name="pmvc:account-detail")
     class Meta:
         model = Account
-        fields = ['url', 'name', 'enabled']
+        fields = ['url', 'id', 'name', 'enabled']
 
 class VideoSerializer(serializers.HyperlinkedModelSerializer):
     url = serializers.HyperlinkedIdentityField(view_name="pmvc:video-detail")
